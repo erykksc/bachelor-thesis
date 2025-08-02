3d791693b203ffb5c81fb0d270dbe341d0f0f7f5
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-08-02 18:06:47 +0200

    chore(opentofu): remove unused files

--- opentofu/aks-cluster/variables.tf
+++ opentofu/aks-cluster/variables.tf
@@ -14,5 +14,5 @@ variable "node_count" {
 variable "vm_size" {
   description = "Azure VM size for AKS nodes"
   type        = string
-  default     = "Standard_D2alds_v6"
+  default     = "Standard_D2ads_v6"
 }
