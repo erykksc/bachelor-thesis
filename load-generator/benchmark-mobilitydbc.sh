88c70333ccd40a660617306404700a3c087711b5
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-08-03 18:13:52 +0200

    feat(load-generator): implement benchmark-mobilitydbc.sh

--- load-generator/benchmark-mobilitydbc.sh
+++ load-generator/benchmark-mobilitydbc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# ALL OF THOSE BENCHMARKS NEED TO BE RUN ON DIFFERENT CLUSTER SIZES
 # ALL OF THOSE BENCHMARKS NEED TO BE RUN BOTH FOR CRATEDB AND MOBILITYDBC
 
 # INSERTS
@@ -20,3 +21,55 @@
 # run different amount of client threads 10,100,1000
 	# insert the data as fast as possible
 
+# Default values
+BATCH_SIZE=2000
+DB='postgresql://postgres:postgres@localhost:5432'
+NWORKERS=16
+NCOMPLEXQRS=100000000000 # 100 billion queries, it should be impossible to perform so that the timeout is reached
+NSIMPLEQRS=100000000000 # 100 billion queries
+TRIPS='../dataset-generator/output/escooter-trips-small.csv'
+QRS_TIMEOUT='25m'
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --db) DB="$2"; shift ;;
+    --batch-size) BATCH_SIZE="$2"; shift ;;
+    --nworkers) NWORKERS="$2"; shift ;;
+    --trips) TRIPS="$2"; shift ;;
+    --queries-timeout) QRS_TIMEOUT="$2"; shift ;;
+    *) echo "Unknown option $1"; exit 1 ;;
+  esac
+  shift
+done
+
+# Init DB
+go run . --mode init \
+  --dbTarget mobilitydbc \
+  --db $DB \
+  --migrations ./migrations/mobilitydbc
+
+# Insert
+go run . --mode insert \
+  --dbTarget mobilitydbc \
+  --db $DB \
+  --nworkers $NWORKERS \
+  --batch-size $BATCH_SIZE \
+  --bulk-insert \
+  --trips $TRIPS
+
+# Simple queries
+timeout --signal=INT $QRS_TIMEOUT go run . --mode query \
+  --dbTarget mobilitydbc \
+  --db $DB \
+  --nworkers $NWORKERS \
+  --queries ./schemas/mobilitydb-simple-read-queries.tmpl \
+  --nqueries $NSIMPLEQRS
+
+# Complex queries
+timeout --signal=INT $QRS_TIMEOUT go run . --mode query \
+  --dbTarget mobilitydbc \
+  --db $DB \
+  --nworkers $NWORKERS \
+  --queries ./schemas/mobilitydb-complex-read-queries.tmpl \
+  --nqueries $NCOMPLEXQRS
