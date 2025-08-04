13eeb14b140898c9a5ae79c177fb1b11a2b5ee88
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2025-08-04 16:00:18 +0200

    feat(load-generator): set sensible defaults in benchmark.sh

--- load-generator/benchmark.sh
+++ load-generator/benchmark.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 # Default values
 DB_TARGET="cratedb"
-BATCH_SIZE=2000
+BATCH_SIZE=2048
 DB_CONN_STR='postgresql://crate:crate@localhost:5432'
-NWORKERS=10
+NWORKERS=16
 NCOMPLEX_QUERIES=100000000000 # 100 billion queries, it should be impossible to perform so that the timeout is reached
 NSIMPLE_QUERIES=100000000000 # 100 billion queries
 TRIPS='../dataset-generator/output/escooter-trips-large.csv'
